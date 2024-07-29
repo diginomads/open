b2def816d4d4731b799fc6bd490598279c4be7a3
Ronak-Ma <ronak@usenomad.ai>
2024-07-29 14:03:18 -0400

    Cal.com Project using Next.js

---
+++ custom.d.ts
@@ -0,0 +1,9 @@
+declare module '@shadcn/ui' {
+    export const Table: any;
+    export const TableHead: any;
+    export const TableBody: any;
+    export const TableRow: any;
+    export const TableCell: any;
+    export const TableCaption: any;
+  }
+  
